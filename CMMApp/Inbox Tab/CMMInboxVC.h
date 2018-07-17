71a016e3602b8147e8b49f440d48ee898403ad2e
Omar Rasheed <omarrasheed@fb.com>
2018-07-17 16:42:05 -0700

    Initalize files

---
+++ CMMApp/Inbox Tab/CMMInboxVC.h
@@ -0,0 +1,13 @@
+//
+//  CMMInboxVC.h
+//  CMMApp
+//
+//  Created by Omar Rasheed on 7/17/18.
+//  Copyright © 2018 Omar Rasheed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMMInboxVC : UIViewController
+
+@end
