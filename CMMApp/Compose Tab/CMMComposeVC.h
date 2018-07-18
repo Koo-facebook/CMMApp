06bcd9c9937479c5d26583103a1b3276f640535b
Keylonnie Miller <keylonniem@fb.com>
2018-07-18 10:52:34 -0700

    Composer Tab Added

---
+++ CMMApp/Compose Tab/CMMComposeVC.h
@@ -0,0 +1,13 @@
+//
+//  CMMComposeVC.h
+//  CMMApp
+//
+//  Created by Keylonnie Miller on 7/18/18.
+//  Copyright © 2018 Omar Rasheed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMMComposeVC : UIViewController
+
+@end
