b9f554080b4f8412ca212f369d5ac8ec49c3141a
Olivia Jorasch <ojorasch@fb.com>
2018-07-20 15:12:42 -0700

    improved readability of code

--- CMMApp/Post Details VC/PostDetailVC.m
+++ CMMApp/Post Details VC/PostDetailVC.m
@@ -13,6 +13,12 @@
 #import "UIImageView+AFNetworking.h"
 
 @interface PostDetailVC ()
+@property (strong, nonatomic) UILabel *authorLabel;
+@property (strong, nonatomic) UILabel *categoryLabel;
+@property (strong, nonatomic) UILabel *dateLabel;
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *detailLabel;
+@property (strong, nonatomic) UIImageView *authorImage;
 @end
 
 @implementation PostDetailVC
@@ -33,80 +39,83 @@
     self.post = post;
     self.title = @"Post Details";
     
-    UILabel *titleLabel = [[UILabel alloc] init];
-    titleLabel.numberOfLines = 0;
-    titleLabel.text = post.topic;
-    [self.view addSubview:titleLabel];
-    [titleLabel setFont:[UIFont systemFontOfSize:16]];
-    NSLog(@"%@", titleLabel.text);
-    
-    UILabel *dateLabel = [[UILabel alloc] init];
-    dateLabel.text = [post.createdAt timeAgoSinceNow];
-    UIColor *tealColor = [UIColor colorWithRed:54/255.f green:173/255.f blue:157/255.f alpha:1];
-    dateLabel.textColor = tealColor;
-    [self.view addSubview:dateLabel];
-    [dateLabel setFont:[UIFont systemFontOfSize:14]];
-    
-    UILabel *categoryLabel = [[UILabel alloc] init];
-    categoryLabel.text = post.category;
-    categoryLabel.textColor = tealColor;
-    [self.view addSubview:categoryLabel];
-    [categoryLabel setFont:[UIFont systemFontOfSize:14]];
-    
-    UILabel *detailLabel = [[UILabel alloc] init];
-    detailLabel.text = post.detailedDescription;
-    detailLabel.numberOfLines = 0;
-    [self.view addSubview:detailLabel];
-    [detailLabel setFont:[UIFont systemFontOfSize:16]];
-    
-    UILabel *authorLabel = [[UILabel alloc] init];
-    authorLabel.text = post.owner.username;
-    authorLabel.textColor = tealColor;
-    [self.view addSubview:authorLabel];
-    [authorLabel setFont:[UIFont systemFontOfSize:14]];
-    
     int topPadding = 80;
     int imageSize = 70;
-    CGRect imageFrame = CGRectMake(12, topPadding, imageSize, imageSize);
-    UIImageView *authorImage = [[UIImageView alloc] initWithFrame:imageFrame];
-    authorImage.image = nil;
-    [authorImage setImageWithURL:[NSURL URLWithString:post.owner.profileImage.url] placeholderImage:[UIImage imageNamed:@"placeholderProfileImage"]];
-    authorImage.layer.cornerRadius = imageSize/2;
-    authorImage.clipsToBounds = YES;
-    [self.view addSubview:authorImage];
     
-    // Autolayout for the labels
+    [self createLabels];
+    [self displayProfileImageWithSize:imageSize padding:topPadding];
+    [self layOutLabelsWithImageSize:imageSize padding:topPadding];
+}
+
+- (void)configureLabel:(UILabel *)label text:(NSString *)text fontSize:(int)fontSize {
+    label.text = text;
+    [self.view addSubview:label];
+    [label setFont:[UIFont systemFontOfSize:fontSize]];
+}
+
+- (void)createLabels {
+    UIColor *tealColor = [UIColor colorWithRed:54/255.f green:173/255.f blue:157/255.f alpha:1];
     
+    self.titleLabel = [[UILabel alloc] init];
+    self.titleLabel.numberOfLines = 0;
+    [self configureLabel:self.titleLabel text:self.post.topic fontSize:16];
+    
+    self.dateLabel = [[UILabel alloc] init];
+    self.dateLabel.textColor = tealColor;
+    [self configureLabel:self.dateLabel text:[self.post.createdAt timeAgoSinceNow] fontSize:14];
+    
+    self.categoryLabel = [[UILabel alloc] init];
+    self.categoryLabel.textColor = tealColor;
+    [self configureLabel:self.categoryLabel text:self.post.category fontSize:14];
+    
+    self.detailLabel = [[UILabel alloc] init];
+    self.detailLabel.numberOfLines = 0;
+    [self configureLabel:self.detailLabel text:self.post.detailedDescription fontSize:16];
+    
+    self.authorLabel = [[UILabel alloc] init];
+    self.authorLabel.textColor = tealColor;
+    [self configureLabel:self.authorLabel text:self.post.owner.username fontSize:14];
+}
+
+- (void)displayProfileImageWithSize:(int)size padding:(int)padding {
+    CGRect imageFrame = CGRectMake(12, padding, size, size);
+    self.authorImage = [[UIImageView alloc] initWithFrame:imageFrame];
+    self.authorImage.image = nil;
+    [self.authorImage setImageWithURL:[NSURL URLWithString:self.post.owner.profileImage.url] placeholderImage:[UIImage imageNamed:@"placeholderProfileImage"]];
+    self.authorImage.layer.cornerRadius = size/2;
+    self.authorImage.clipsToBounds = YES;
+    [self.view addSubview:self.authorImage];
+}
+
+- (void)layOutLabelsWithImageSize:(int)imageSize padding:(int)topPadding {
     UIEdgeInsets titlePadding = UIEdgeInsetsMake(topPadding + imageSize + 36, 12, 12, 12);
-    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(titlePadding.top);
         make.left.equalTo(self.view.mas_left).with.offset(titlePadding.left);
-        //make.height.equalTo(@(titleLabel.intrinsicContentSize.height));
         make.right.equalTo(self.view.mas_right).with.offset(-titlePadding.right);
     }];
     UIEdgeInsets categoryPadding = UIEdgeInsetsMake(topPadding + imageSize + 12, 12, 12, 12);
-    [categoryLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.categoryLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(categoryPadding.top);
         make.left.equalTo(self.view.mas_left).with.offset(categoryPadding.left);
-        make.width.equalTo(@(categoryLabel.intrinsicContentSize.width));
+        make.width.equalTo(@(self.categoryLabel.intrinsicContentSize.width));
     }];
     UIEdgeInsets datePadding = UIEdgeInsetsMake(topPadding + imageSize + 12, 12, 12, 12);
-    [dateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.dateLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(datePadding.top);
-        make.left.equalTo(categoryLabel.mas_right).with.offset(datePadding.left);
+        make.left.equalTo(self.categoryLabel.mas_right).with.offset(datePadding.left);
         make.right.equalTo(self.view.mas_right).with.offset(-datePadding.right);
     }];
     UIEdgeInsets detailPadding = UIEdgeInsetsMake(12, 12, 12, 12);
-    [detailLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(titleLabel.mas_bottom).with.offset(detailPadding.top);
+    [self.detailLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.titleLabel.mas_bottom).with.offset(detailPadding.top);
         make.left.equalTo(self.view.mas_left).with.offset(detailPadding.left);
         make.right.equalTo(self.view.mas_right).with.offset(-detailPadding.right);
-        //make.height.equalTo(@(detailLabel.intrinsicContentSize.height));
     }];
     UIEdgeInsets authorPadding = UIEdgeInsetsMake(topPadding + imageSize/2, 12, 12, 12);
-    [authorLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.authorLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.view.mas_top).with.offset(authorPadding.top);
-        make.left.equalTo(authorImage.mas_right).with.offset(authorPadding.left);
+        make.left.equalTo(self.authorImage.mas_right).with.offset(authorPadding.left);
         make.right.equalTo(self.view.mas_right).with.offset(-authorPadding.right);
     }];
 }
